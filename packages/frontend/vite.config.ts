8a1171bc8eb41d956831dd309e5a77ab77b937e3
seaeast2 <seaeast2@gmail.com>
2026-02-27 13:09:27 +0900

    ssulmantool 로 dns 업데이트

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3002,
-        allowedHosts: ['webdev99.duckdns.org'],
+        allowedHosts: ['ssulmantool.duckdns.org'],
         proxy: {
             '/api': {
                 target: 'http://localhost:3001',
